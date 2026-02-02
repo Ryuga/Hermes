83290628a15edae5e60451d23a1cb94fa9c0c855
Ryuga <vaishnav2353@gmail.com>
2026-02-03 00:22:53 +0530

    Hello world

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use tokio::net::TcpListener;
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> &'static str {
+    "Hello world!"
+}
