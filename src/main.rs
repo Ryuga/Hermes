0ef056043a762c85afc059b42b569df86ca65195
Ryuga <vaishnav2353@gmail.com>
2026-02-03 23:18:19 +0530

    add .env

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,23 @@ mod runner;
 
 use tokio::net::TcpListener;
 use axum::{Router, routing::{get, post}, Json};
-use std::net::SocketAddr;
 use models::Req;
 use crate::exe::execute_code;
+use dotenvy::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+    let port = env::var("PORT").unwrap_or("8000".into());
+    let host = env::var("HOST").unwrap_or("127.0.0.1".into());
 
     let app = Router::new()
         .route("/", get(handler))
         .route("/execute/", post(execution_handler));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let addr = format!("{}:{}", host, port);
+    let listener = TcpListener::bind(&addr).await.unwrap();
 
     println!("listening on {}", addr);
     axum::serve(listener, app).await.unwrap();
