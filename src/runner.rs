33ded35908ea3790dc0fe1c303a655e02912cf1e
Ryuga <vaishnav2353@gmail.com>
2026-02-05 00:11:39 +0530

    Update runner with tested command

--- src/runner.rs
+++ src/runner.rs
@@ -1,21 +1,39 @@
 use std::path::Path;
-use std::process::{Command};
+use std::process::Command;
 use crate::lang_config::LangConfig;
 
 pub fn safe_execute(work_dir: &Path, config: LangConfig) -> Result<(String, String, i32), String> {
-    let mut cmd = Command::new("nsjail");
+    let mut cmd = Command::new("sudo");
+
+    cmd.arg("nsjail");
     cmd.args([
-        "--disable_clone_newns",
+        "--mode", "o",
         "--time_limit", "2",
-        "--rlimit_cpu", "2",
+        "--max_cpus", "1",
         "--rlimit_as", "256",
+        "--rlimit_cpu", "2",
+        "--rlimit_fsize", "5",
+        "--rlimit_nofile", "32",
+        "--rlimit_nproc", "16",
         "--disable_proc",
         "--iface_no_lo",
+        "--user", "99999",
+        "--group", "99999",
+        "--cwd", "/sandbox",
+    ]);
+
+    cmd.arg("--bindmount");
+    cmd.arg(format!("{}:/sandbox", work_dir.display()));
+
+    cmd.args([
+        "--bindmount_ro", "/usr",
+        "--bindmount_ro", "/lib",
+        "--bindmount_ro", "/lib64",
+        "--bindmount_ro", "/bin",
         "--",
     ]);
 
-    cmd.args(config.run);
-    cmd.current_dir(work_dir);
+    cmd.args(&config.run);
     cmd.stdin(std::process::Stdio::piped());
     cmd.stdout(std::process::Stdio::piped());
     cmd.stderr(std::process::Stdio::piped());
