2dad5d36942b0714645cfa1e3780ae657868f67b
Ryuga <vaishnav2353@gmail.com>
2026-02-05 15:35:52 +0530

    Remove sudo execution, Load more language specific configuration from json with defaults.

--- src/models.rs
+++ src/models.rs
@@ -1,10 +1,40 @@
 use serde::{Deserialize, Serialize};
 
+
+fn default_time_limit() -> u64 { 2 }
+fn default_cpu_time_sec() -> u64 { 2 }
+fn default_memory_mb() -> u64 { 256 }
+fn default_stack_mb() -> u64 { 16 }
+fn default_processes() -> u64 { 8 }
+fn default_open_files() -> u64 { 64 }
+fn default_output_kb() -> u64 { 1024 }
+
 #[derive(Deserialize, Clone)]
 pub struct LangConfig {
     pub source: String,
+
     pub run: Vec<String>,
+
+    #[serde(default = "default_time_limit")]
     pub max_time_limit: u64,
+
+    #[serde(default = "default_cpu_time_sec")]
+    pub max_cpu_time_sec: u64,
+
+    #[serde(default = "default_memory_mb")]
+    pub max_memory_mb: u64,
+
+    #[serde(default = "default_stack_mb")]
+    pub max_stack_mb: u64,
+
+    #[serde(default = "default_processes")]
+    pub max_processes: u64,
+
+    #[serde(default = "default_open_files")]
+    pub max_open_files: u64,
+
+    #[serde(default = "default_output_kb")]
+    pub max_output_kb: u64,
 }
 
 #[derive(Deserialize)]
--- src/runner.rs
+++ src/runner.rs
@@ -1,41 +1,54 @@
+use std::env;
 use std::path::Path;
 use std::process::Command;
 use crate::models::LangConfig;
 
-pub fn safe_execute(work_dir: &Path, config: LangConfig) -> Result<(String, String, i32), String> {
-    let mut cmd = Command::new("sudo");
-    let time_limit = config.max_time_limit.to_string();
 
-    cmd.arg("nsjail");
+pub fn safe_execute(work_dir: &Path, config: LangConfig) -> Result<(String, String, i32), String> {
+    let debug = env::var("DEBUG").unwrap_or_else(|_|"false".to_string()) == "true";
+    let fsize_mb = ((config.max_output_kb + 1023) / 1024).max(1);
+
+    let mut cmd = Command::new("nsjail");
+
     cmd.args([
         "--mode", "o",
-        "--time_limit", &time_limit,
-        "--max_cpus", "1",
-        "--rlimit_as", "256",
-        "--rlimit_cpu", "2",
-        "--rlimit_fsize", "5",
-        "--rlimit_nofile", "32",
-        "--rlimit_nproc", "16",
-        "--disable_proc",
-        "--iface_no_lo",
-        "--user", "99999",
-        "--group", "99999",
         "--cwd", "/sandbox",
-        "--really_quiet",
+        "--max_cpus", "1",
     ]);
 
-    cmd.arg("--bindmount");
-    cmd.arg(format!("{}:/sandbox", work_dir.display()));
+    if  !debug {
+        cmd.arg("--really_quiet");
+    }
+    cmd.arg("--uid_mapping").arg(format!("0:{}:1", nix::unistd::getuid()));
+    cmd.arg("--gid_mapping").arg(format!("0:{}:1", nix::unistd::getgid()));
+
+    cmd.arg("--time_limit").arg(config.max_time_limit.to_string());
+    cmd.arg("--rlimit_as").arg(config.max_memory_mb.to_string());
+    cmd.arg("--rlimit_cpu").arg(config.max_cpu_time_sec.to_string());
+    cmd.arg("--rlimit_nofile").arg(config.max_open_files.to_string());
+    cmd.arg("--rlimit_nproc").arg(config.max_processes.to_string());
+    cmd.arg("--rlimit_stack").arg(config.max_stack_mb.to_string());
+    cmd.arg("--rlimit_fsize").arg(fsize_mb.to_string());
+
+    cmd.arg("--bindmount").arg(format!("{}:/sandbox", work_dir.display()));
+
+
 
     cmd.args([
         "--bindmount_ro", "/usr",
         "--bindmount_ro", "/lib",
         "--bindmount_ro", "/lib64",
         "--bindmount_ro", "/bin",
+        "--disable_proc",
+        "--iface_no_lo",
+        "--env", "PATH=/usr/bin:/bin",
         "--",
     ]);
 
+
+
     cmd.args(&config.run);
+
     cmd.stdin(std::process::Stdio::piped());
     cmd.stdout(std::process::Stdio::piped());
     cmd.stderr(std::process::Stdio::piped());
